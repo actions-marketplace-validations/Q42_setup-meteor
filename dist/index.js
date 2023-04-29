3429a0811a48735c1c9687c625ac4df12cdd7dde
EmJee <mart-janroeleveld@outlook.com>
2023-04-29 11:39:28 +0200

    Create new build

--- dist/index.js
+++ dist/index.js
@@ -3967,12 +3967,13 @@ async function getMeteorVersion() {
         return versionInput;
     }
     const versionFilePath = node_path_1.default.join(process.env.GITHUB_WORKSPACE, versionFileInput);
-    try {
-        await fs.access(versionFilePath, fs.constants.F_OK);
-    }
-    catch (_) {
-        throw new Error(`The specified meteor-version-file is not readable or does not exist at "${versionFilePath}"`);
-    }
+    // try {
+    await fs.access(versionFilePath, fs.constants.F_OK);
+    // } catch (_) {
+    // 	throw new Error(
+    // 		`The specified meteor-version-file is not readable or does not exist at "${versionFilePath}"`
+    // 	)
+    // }
     const fileContents = await fs.readFile(versionFilePath, 'utf-8');
     const [, version] = fileContents.split('@');
     if (!version) {
