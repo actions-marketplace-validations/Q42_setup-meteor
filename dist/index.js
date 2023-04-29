e2a849bfede9fe2eaf5dcd42a523d13ea752129b
EmJee <mart-janroeleveld@outlook.com>
2023-04-29 11:53:26 +0200

    Replace constants with number

--- dist/index.js
+++ dist/index.js
@@ -3968,7 +3968,7 @@ async function getMeteorVersion() {
     }
     const versionFilePath = node_path_1.default.join(process.env.GITHUB_WORKSPACE, versionFileInput);
     // try {
-    await fs.access(versionFilePath, fs.constants.F_OK);
+    await fs.access(versionFilePath, 0 /* fs.constants.F_OK */);
     // } catch (_) {
     // 	throw new Error(
     // 		`The specified meteor-version-file is not readable or does not exist at "${versionFilePath}"`
